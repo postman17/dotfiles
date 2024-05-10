d48a97f258015ed0828f727441534d97b97e9b0c
Константин Морозов <konstantin.morozov@vk.team>
2024-05-10 15:31:42 +0300

    added neogit and yazi

--- nvim/nvim/lua/core/plugins.lua
+++ nvim/nvim/lua/core/plugins.lua
@@ -74,5 +74,17 @@ require("lazy").setup({
 },
 {'tomasky/bookmarks.nvim'},
 {'petertriho/nvim-scrollbar'},
-{'kevinhwang91/nvim-hlslens'}
+{'kevinhwang91/nvim-hlslens'},
+{
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+  },
+  config = true
+}
 })
