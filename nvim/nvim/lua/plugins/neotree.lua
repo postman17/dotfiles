7f18d0057f4662c76d98235ece71a3a54a966901
konstantin.morozov <konstantin.morozov@vk.team>
2024-10-29 11:44:59 +0300

    nvim live greap

--- nvim/nvim/lua/plugins/neotree.lua
+++ nvim/nvim/lua/plugins/neotree.lua
@@ -8,24 +8,49 @@ vim.fn.sign_define("DiagnosticSignHint",
 	{text = "󰌵", texthl = "DiagnosticSignHint"})
 
 
-local actions = require "telescope.actions"
-require("telescope").setup {
-	defaults = {
-        mappings = {
-          i = {
-            ["<C-n>"] = actions.cycle_history_next,
-            ["<C-p>"] = actions.cycle_history_prev,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-S-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<ScrollWheelUp>"] = actions.preview_scrolling_up,
-		    ["<ScrollWheelDown>"] = actions.preview_scrolling_down,
-			["<C-f>"] = actions.results_scrolling_up,
-			["<C-b>"] = actions.results_scrolling_down,
-          },
-          n = { 
-		  q = actions.close
-	  },
-        },
-   }
-}
+local function getTelescopeOpts(state, path)
+  return {
+    cwd = path,
+    search_dirs = { path },
+    attach_mappings = function (prompt_bufnr, map)
+      local actions = require "telescope.actions"
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local action_state = require "telescope.actions.state"
+        local selection = action_state.get_selected_entry()
+        local filename = selection.filename
+        if (filename == nil) then
+          filename = selection[1]
+        end
+        -- any way to open the file without triggering auto-close event of neo-tree?
+        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+      end)
+      return true
+    end
+  }
+end
+
+
+require("neo-tree").setup({
+   filesystem = {
+    window = {
+      mappings = {
+        ["tf"] = "telescope_find",
+        ["tg"] = "telescope_grep",
+	['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
+      },
+    },
+  },
+  commands = {
+    telescope_find = function(state)
+      local node = state.tree:get_node()
+      local path = node:get_id()
+      require('telescope.builtin').find_files(getTelescopeOpts(state, path))
+    end,
+    telescope_grep = function(state)
+      local node = state.tree:get_node()
+      local path = node:get_id()
+      require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
+    end,
+  },
+})
